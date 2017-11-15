b35a517470aae560434e7eb2b94bf51f5cf3ac67
musitive <dallinfrank@gmail.com>
2017-11-15 16:23:43 -0700

    fdsa

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/commentDB',{useMongoClient:true});
-require('./models/Comments');
+require('./models/Headlines');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
